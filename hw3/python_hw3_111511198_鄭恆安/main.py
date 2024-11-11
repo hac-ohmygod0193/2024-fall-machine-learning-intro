cb1cd0f99d23068b7d0fa68c51ed299492943f81
hac-ohmygod0193 <alex.chenghengan@gmail.com>
2024-11-11 22:22:24 +0800

    hw3 Scoring reach 0.57719

--- hw3/python_hw3_111511198_鄭恆安/main.py
+++ hw3/python_hw3_111511198_鄭恆安/main.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 from preprocessor import Preprocessor
 from model import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, matthews_corrcoef, roc_auc_score
 import os
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def dataPreprocessing(threshold=0.1):
@@ -24,48 +27,46 @@ def dataPreprocessing(threshold=0.1):
 
     train_X = Preprocessor(train_X).preprocess()
     test_X = Preprocessor(test_X).preprocess()
-    print(train_X.columns)
-    print(train_y.columns)
-    print(test_X.columns)
+    
     # use feature_selection to select the feature
     
     # concat train_X and train_y
     train_data = pd.concat([train_X, train_y], axis=1)
     train_data = train_data.fillna(train_data.mean()) # 0 here stands for np.mean() in original data
     selected_column = Preprocessor(train_data).feature_selection(threshold)
-    train_data = train_data[selected_column]
 
+    train_data = train_data[selected_column]
     # balance the data
     train_data = Preprocessor(train_data).oversample_minority_class()
     
+    # train data
+    train_X = train_data.drop(columns=['label'])
+    train_y = pd.DataFrame(train_data['label'])
     # test data
-    test_X = test_X.fillna(test_X.mean())
+    test_X = test_X[selected_column[:-1]].fillna(test_X.mean())
 
     # remove the label column, because we don't have test_y
-    test_X = test_X[selected_column[:-1]] 
+    #test_X = test_X[selected_column[:-1]] 
 
     # print(train_data.head())
+    
     if 'Unnamed: 0' in train_X.columns:
         train_X = train_X.drop(columns=['Unnamed: 0'])
-    if 'Unnamed: 0' in train_y.columns:
-        train_y = train_y.drop(columns=['Unnamed: 0'])
     if 'Unnamed: 0' in test_X.columns:
         test_X = test_X.drop(columns=['Unnamed: 0'])
+        
     
     
     return train_X, train_y, test_X # train, test data should be numpy array
 
 
 
-
 def main():
     train_X, train_y, test_X = dataPreprocessing(0.1) # train, test data should not contain index
-    print(train_X.columns)
-    print(train_y.columns)
-    print(test_X.columns)
-    Decision_tree = DecisionTreeClassifier(max_depth=1)
-    Decision_tree.fit(train_X,train_y)
-    Decision_tree.print_tree()
+    
+    model = DecisionTreeClassifier(max_depth=5, min_samples_leaf=1, method='entropy')
+    model.fit(train_X,train_y)
+    model.print_tree()
     '''
     # TODO 
     # build your decision tree
@@ -76,6 +77,14 @@ def main():
     root_path = './' # change the root path
     pred.to_csv(root_path+'pred_hw3_111511198_鄭恆安.csv', index=False, header=False)
     '''
+    train_pred = model.predict(train_X)
+    train_y = np.array(train_y['label'])
+
+    pred = model.predict(test_X)
+    test_y = pd.read_csv('./proj3_data/test_y.csv')
+    test_y = np.array(test_y['label'])
+    model.predict_score(train_pred, train_y)
+    model.predict_score(pred, test_y)
 
 if __name__ == "__main__":
     np.random.seed(0)
--- hw3/python_hw3_111511198_鄭恆安/model.py
+++ hw3/python_hw3_111511198_鄭恆安/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from abc import ABC, abstractmethod
 import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, matthews_corrcoef, roc_auc_score
 
 
 # Base classifier class
@@ -20,118 +21,180 @@ class Classifier(ABC):
         # Abstract method predict the probability of the dataset X
         pass
 class TreeNode:
-    def __init__(self, feature, threshold, depth, data, left=None, right=None):
-        self.feature = feature
-        self.threshold = threshold
-        self.depth = depth
-        self.data = data
-        self.zero = None
-        self.one = None
-        self.left = left
-        self.right = right
-    def create_leaf(self, data, depth):
+    def __init__(self, data, depth):
         self.data = data
         self.zero = np.sum(data == 0)
         self.one = np.sum(data == 1)
 
+    def create_node(self, feature, threshold):
+        self.attr = 'node'
+        self.feature = feature
+        self.threshold = threshold
+        self.left = None
+        self.right = None
+        
+    def create_leaf(self):
+        self.attr = 'leaf'
+        if(self.zero > self.one):
+            self.value = 0
+        else:
+            self.value = 1
+    def predict_result(self, x):
+        if self.left is None and self.right is None:
+            return np.argmax([self.zero, self.one])
+        if x[self.feature] < self.threshold:
+            return self.left.predict_result(x)
+        else:
+            return self.right.predict_result(x)
+
 class DecisionTreeClassifier:
-    def __init__(self, max_depth=5):
+    def __init__(self, max_depth=5,min_samples_leaf=1,method='entropy'):
         self.max_depth = max_depth
         self.tree = None
-        self.min_samples_leaf = 1
+        self.min_samples_leaf = min_samples_leaf
         self.min_ig = 0.0
+        self.method = method
     def fit(self, X, y):
         y = np.array(y['label'])
         self.tree = self._grow_tree(X, y)
-
     def _grow_tree(self, X, y, depth=0):
-        
-        if depth > self.max_depth or np.unique(y).shape[0] == 1:
-            return None
+        node = TreeNode(y, depth)
+        if depth > self.max_depth or len(y) < self.min_samples_leaf or np.unique(y).shape[0] == 1:
+            node.create_leaf()
+            return node
         # find the best split
-        feature, threshold, entropy = self.find_best_split(X, y)
+        feature, threshold = self.find_best_split(X, y)
         # split the dataset
-        y_left, y_right, X = self.split_dataset(X, y, feature, threshold)
+        X_left, X_right, y_left, y_right = self.split_dataset(X, y, feature, threshold)
         
-
         # grow the tree
-        node = TreeNode(feature, threshold, depth, y)
-        print(len(X), len(y_left), len(y_right))
-        node.left = self._grow_tree(X, y_left, depth + 1)
-        node.right = self._grow_tree(X, y_right, depth + 1)
+        node.create_node(feature, threshold)
+        #print(len(y_left), len(y_right), len(y))
+        node.left = self._grow_tree(X_left, y_left, depth + 1)
+        node.right = self._grow_tree(X_right, y_right, depth + 1)
         return node
 
     # Split dataset based on a feature and threshold
     def split_dataset(self, X, y, feature_index, threshold):
-        y_left, y_right = [], []
-        for i in range(len(X[feature_index])):
-            if X[feature_index][i] < threshold:
-                y_left.append(y[i])
-            else:
-                y_right.append(y[i])
-        X = X.drop(columns=[feature_index])
-        return y_left, y_right, X
+
+        left_indices = X[feature_index] < threshold
+        right_indices = X[feature_index] >= threshold
+
+        X_left = X[left_indices]
+        X_right = X[right_indices]
+        y_left = y[left_indices]
+        y_right = y[right_indices]
+
+        X_left = X_left.drop(columns=[feature_index])
+        X_right = X_right.drop(columns=[feature_index])
+        return X_left, X_right, y_left, y_right
 
 
     # Find the best split for the dataset
     def find_best_split(self, X, y):
         # X is dataframes
         # y is numpy array
-        min_entropy = float('inf')
+        max_gain_ratio = float('-inf')
         best_feature, best_threshold = None, None
         for feature_index in X.columns:
-            print(f'Feature: {feature_index}')
+            #print(f'Feature: {feature_index}')
             feature_values = X[feature_index].values
             for threshold in np.unique(feature_values):
                 y_left,y_right = [], []
-                print("threshold", threshold, len(feature_values), len(y))
+                #print("threshold", threshold, len(feature_values), len(y))
                 for i in range(len(feature_values)):
                     if feature_values[i] < threshold:
                         y_left.append(y[i])
                     else:
                         y_right.append(y[i])
-                print(len(y_left), len(y_right))
-                entropy_left = self.entropy(y_left)
-                entropy_right = self.entropy(y_right)
-                entropy_ = (len(y_left) / len(y)) * entropy_left + (len(y_right) / len(y)) * entropy_right
-                if entropy_ < min_entropy:
-                    min_entropy = entropy_
+                gain_ratio = self._gain_ratio(y, y_left, y_right)
+                if gain_ratio > max_gain_ratio:
+                    max_gain_ratio = gain_ratio
                     best_feature = feature_index
                     best_threshold = threshold
-        print(f'Best feature: {best_feature}, Best threshold: {best_threshold}, Min entropy: {min_entropy}')
-        return best_feature, best_threshold, min_entropy
+                
+        print(f'Best feature: {best_feature}, Best threshold: {best_threshold}, Max Gain Ratio: {max_gain_ratio}')
+        return best_feature, best_threshold
     def entropy(self, y):
         # Calculate the entropy of the dataset
         return -np.sum([(np.sum(y == c) / len(y)) * np.log2(np.sum(y == c) / len(y)) for c in np.unique(y)])
-    
+    def gini(self, y):
+        # Calculate the gini impurity of the dataset
+        return 1 - np.sum([(np.sum(y == c) / len(y)) ** 2 for c in np.unique(y)])
+    def _information_gain(self, y, y_left, y_right):
+        # Calculate the information gain of a split
+        if self.method=='entropy':
+            ig = self.entropy(y) - (len(y_left) / len(y)) * self.entropy(y_left) - (len(y_right) / len(y)) * self.entropy(y_right)
+        if self.method=='gini':
+            ig = self.gini(y) - (len(y_left) / len(y)) * self.gini(y_left) - (len(y_right) / len(y)) * self.gini(y_right)
+        return ig
+    def _split_information(self,y_left, y_right):
+        """Calculate Split Information."""
+        n_left, n_right = len(y_left), len(y_right)
+        n_total = n_left + n_right
+        p_left, p_right = n_left / n_total, n_right / n_total
 
+        # Split information (Entropy of the split proportions)
+        return -sum(p * np.log2(p) for p in [p_left, p_right] if p > 0)
+
+    def _gain_ratio(self,y, y_left, y_right):
+        """Calculate Gain Ratio."""
+        ig = self._information_gain(y, y_left, y_right)
+        si = self._split_information(y_left, y_right)
+        
+        # Avoid division by zero
+        return ig / si if si != 0 else 0
     # prediction
     def predict_proba(self, X):
         raise NotImplementedError
-
+    def predict_score(self, pred, y):
+        acc = accuracy_score(pred, y)
+        f1 = f1_score(pred, y, zero_division=0)
+        mcc = matthews_corrcoef(pred, y)
+        print(pred)
+        #print(y)
+        print('-' * 50)
+        print(f'Acc: {acc:.5f}')
+        print(f'F1 score: {f1:.5f}')
+        print(f'MCC: {mcc:.5f}')
+        scoring = 0.3 * acc + 0.35 * f1 + 0.35 * mcc
+        print(f'Scoring: {scoring:.5f}')
+        print('-' * 50)
     def predict(self, X):
-        raise NotImplementedError
+        if self.tree is None:
+            raise ValueError("Tree is empty")
+        preds = []
+        for i in range(len(X)):
+            preds.append(self._predict_tree(X.iloc[i], self.tree))
+        return preds
 
-    def _predict_tree(self, x, tree_node):
-        raise NotImplementedError
-    
+    def _predict_tree(self, X, node):
+        if node.attr == 'leaf':
+            return node.value
+        if X[node.feature] < node.threshold:
+            return self._predict_tree(X, node.left)
+        else:
+            return self._predict_tree(X, node.right)
 
     # print tree
-    def print_tree(self, max_print_depth=3):
+    def print_tree(self):
         if self.tree is None:
             print("Tree is empty")
             return
-        if self.tree.depth > max_print_depth:
-            return
         self._print_tree(self.tree)
-    def _print_tree(self, node, depth=0):
+    def _print_tree(self, node, depth=0, max_print_depth=2):
+        
         if node is None:
             return
-        print(f'{"  " * depth}[{node.feature}] [{node.zero} 0 / {node.one} 1]')
-        print(f'{"  " * depth}Left:')
-        self._print_tree(node.left, depth + 1)
-        print(f'{"  " * depth}Right:')
-        self._print_tree(node.right, depth + 1)
-
-
+        if node.attr == 'node':
+            print(f'{"  " * depth}[{node.feature}] [{node.zero} 0 / {node.one} 1]')
+        else:
+            print(f'{"  " * depth}[{node.value}]')
+        if depth >= max_print_depth or node.attr == 'leaf':
+            return
+        if(node.left is not None and node.right is not None):
+            print(f'{"  " * depth}Left:')
+            self._print_tree(node.left, depth + 1)
+            print(f'{"  " * depth}Right:')
+            self._print_tree(node.right, depth + 1)
   
